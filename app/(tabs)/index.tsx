0b2f8739c4d38e15ae1e44cba023871f09e8be1d
Lee Yujin <yuwls1102@gmail.com>
2026-04-22 13:00:30 +0900

    feat: 임시커밋

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
 // app/(tabs)/index.tsx
 import React from "react";
-import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
 
 // const WEB_URL = "http://10.0.2.2:3000/"; // Android Emulator -> Mac localhost
 // const WEB_URL = "http://10.123.195.158:3000/"; // Android 기기로 연결했을때?
 const WEB_URL = "https://saltbread-map.vercel.app/"; // Android 기기로 연결했을때?
-
-
 
 export default function HomeScreen() {
   return (
